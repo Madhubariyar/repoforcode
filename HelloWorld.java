3e6b5815bd590f3702b66821007213e77e09ab38
Madhubariyar <161322910+Madhubariyar@users.noreply.github.com>
2026-04-23 21:23:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, batch43 team with  madhu sudesna arya ");
-        System.out.println("Hello batch 42 with anurag ahaan avinash "):
+        System.out.println("Hello batch 42 with anurag ahaan avinash ");
     }
 }
